cf24b261e5a96c9588e04efec9899507a8fd12ef
kushtrimh <kushtrimmh13@gmail.com>
2020-11-01 21:30:48 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kushtrimh/loquacious
+
+go 1.15
---
+++ loquacious.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// import "github.com/kushtrimh/loquacious/twitter"
+
+func main() {
+	fmt.Println("Main")
+}
