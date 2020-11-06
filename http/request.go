0035055de84dd2fae8851aed2ceaf3a96f693323
kushtrimh <kushtrimmh13@gmail.com>
2020-11-06 20:16:33 +0100

    Add http requests functions

--- http/request.go
+++ http/request.go
@@ -1,1 +1,41 @@
 package http
+
+import (
+	"github.com/kushtrimh/loquacious/auth"
+	"log"
+	"net/http"
+	"net/url"
+	"path"
+)
+
+type ApiClient struct {
+	client   *http.Client
+	endpoint string
+	auth     *auth.Auth
+}
+
+func NewApiClient(endpoint string, auth *auth.Auth) *ApiClient {
+	client := &http.Client{}
+	return &ApiClient{client, endpoint, auth}
+}
+
+func (api *ApiClient) Get(path string) (*http.Response, error) {
+	req, err := http.NewRequest("GET", api.mergePath(path), nil)
+	if err != nil {
+		return nil, err
+	}
+	req.SetBasicAuth(api.auth.Id, api.auth.Secret)
+	response, err := api.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+func (api *ApiClient) mergePath(urlPath string) string {
+	url, err := url.Parse(api.endpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return path.Join(url.Path, urlPath)
+}
--- http/twitter.go
+++ http/twitter.go
@@ -1,1 +1,3 @@
 package http
+
+const ApiEndpoint string = "https://api.twitter.com/1.1"
